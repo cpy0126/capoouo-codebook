56410d67a38e2127edbc040d98601a219653502f
cpy0126 <0126morris@gmail.com>
2021-11-26 14:52:57 +0800

    ISAP vimrc

--- 1_Basic/vimrc.cpp
+++ 1_Basic/vimrc.cpp
@@ -3,9 +3,10 @@ se nu ai hls et ru ic is sc cul
 se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
 syn on
 hi cursorline cterm=none ctermbg=89
-map {<ENTER> {<CR>}<Esc>ko<TAB>
-map <F8> <Esc>:w<CR>:!g++ "%" -o "%<" -std=c++17 -Wall -Wextra
--Wshadow -Wfatal-errors -Wconversion -fsanitize=address,undefined
--g && echo success<CR>
-map <F9> <Esc>:w<CR>:!g++ "%" -o "%<" -std=c++17 && echo success<CR>
+inoremap {<ENTER> {<CR>}<Esc>ko<TAB>
+map <F8> <Esc>:w<CR>:!g++ "%" -o "%<" -std=c++17 -Wall
+-Wextra -Wshadow -Wconversion -fsanitize=address,
+undefined -g && echo success<CR>
+map <F9> <Esc>:w<CR>:!g++ "%" -o "%<" -std=c++17
+&& echo success<CR>
 map <F10> <Esc>:!./"%<"<CR>
--- 4_Flow_Matching/isap.cpp
+++ 4_Flow_Matching/isap.cpp
@@ -49,4 +49,4 @@ struct Maxflow {
       ;
     return res;
   }
-} flow;
+};

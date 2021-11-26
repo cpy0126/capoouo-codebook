82d313de117ed4e20e667c2bb86e5508357aee7a
cpy0126 <0126morris@gmail.com>
2021-11-26 18:32:32 +0800

    prime factorization

---
+++ 6_Math/Prime_Factorization.cpp
@@ -0,0 +1,13 @@
+vector<pll> f(ll x){
+  vector<pll> factors;
+  for(ll i = 2; i * i <= x; i++)
+    if(x % i == 0){
+      ll a = 0;
+      for(; x % i == 0; x /= i, a++);
+      factors.pb(make_pair(i, a));
+    }
+    if(x > 1) factors.push_back(make_pair(x, 1));
+  return factors;
+}
+
+

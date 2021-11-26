21682989eb2e91e3db5021c0b5a016458d79eeaa
cpy0126 <0126morris@gmail.com>
2021-11-26 17:34:46 +0800

    PDF format

--- 1_Basic/black_magic.cpp
+++ 1_Basic/black_magic.cpp
@@ -2,15 +2,15 @@
 #include <ext/pb_ds/assoc_container.hpp> //rb_tree
 using namespace __gnu_pbds;
 struct myhash {
-    const int RANDOM = (long long)(make_unique<char>().get()) ^ chrono::high_resolution_clock::now().time_since_epoch().count();
-    static unsigned long long hash_f(unsigned long long x) {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-    static unsigned hash_combine(unsigned a, unsigned b) { return a * 31 + b; }
-    ll operator()(ll x) const { return hash_f(x)^RANDOM; }
+  const int RANDOM = (ll)(make_unique<char>().get()) ^ chrono::high_resolution_clock::now().time_since_epoch().count();
+  static unsigned ll hash_f(unsigned ll x) {
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+  static unsigned hash_combine(unsigned a, unsigned b){ return a * 31 + b; }
+  ll operator()(ll x) const{ return hash_f(x)^RANDOM; }
 };
 typedef gp_hash_table<ll, int, myhash> hashtable;
 typedef __gnu_pbds::priority_queue<int> heap;
--- 1_Basic/read.cpp
+++ 1_Basic/read.cpp
@@ -6,8 +6,8 @@ inline char readchar() {
   return *p++;
 }
 inline int readint(){
-	int f=1,x=0;char ch;
-	do{ch=getchar();if(ch=='-')f=-1;}while(ch<'0'||ch>'9');
-	do{x=x*10+ch-'0';ch=getchar();}while(ch>='0'&&ch<='9');
+	int f=1,x=0;char c;
+	do{c=getchar();if(c=='-')f=-1;}while(c<'0'||c>'9');
+	do{x=x*10+c-'0';c=getchar();}while(c>='0'&&c<='9');
 	return f*x;
 }

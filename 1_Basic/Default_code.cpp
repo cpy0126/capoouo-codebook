250295d1ff41aea032fc99a0b542713cd3a3c324
cpy0126 <0126morris@gmail.com>
2022-08-28 22:04:13 +0800

    Add and Mod Basic

--- 1_Basic/Default_code.cpp
+++ 1_Basic/Default_code.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 #define f first
 #define s second
-#define sz(i) ((int)i.size())
-#define all(i) i.begin(),i.end()
+#define SZ(i) ((int)i.size())
+#define All(i) i.begin(),i.end()
 #define pb push_back
 #define endl '\n'
 int main(){
-	ios:sync_with_stdio(false),cin.tie(0),cout.tie(0);
+  ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
 }
--- 1_Basic/vimrc.cpp
+++ 1_Basic/vimrc.cpp
@@ -1,12 +1,7 @@
 "This file should be placed at ~/.vimrc"
 se nu ai hls et ru ic is sc cul
-se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
+se re=1 ts=2 sts=2 sw=2 ls=2 mouse=a
 syn on
 hi cursorline cterm=none ctermbg=89
 inoremap {<ENTER> {<CR>}<Esc>ko<TAB>
-map <F8> <Esc>:w<CR>:!g++ "%" -o "%<" -std=c++17 -Wall
--Wextra -Wshadow -Wconversion -fsanitize=address,
-undefined -g && echo success<CR>
-map <F9> <Esc>:w<CR>:!g++ "%" -o "%<" -std=c++17
-&& echo success<CR>
-map <F10> <Esc>:!./"%<"<CR>
+
